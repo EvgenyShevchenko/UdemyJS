1af70360fa9b3afb864d68f2bb325a0a9cfa1f46
Evgeny Shevchenko <royalsalsk@gmail.com>
2021-01-28 00:31:12 +0300

    feat : add cycle rating and rating of watched films

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 "use strict";
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+
 let personalMoveDB = {
     count: numberOfFilms,
     movies: {},
@@ -8,11 +10,27 @@ let personalMoveDB = {
     genres: [],
     privat: false
 };
-const film1 = prompt('Один из последних просмотренных фильмов?',''),
-      rating1 = prompt('На сколько оцените его?',''),
-      film2 = prompt('Один из последних просмотренных фильмов?',''),
-      rating2 = prompt('На сколько оцените его?','');
 
-personalMoveDB.movies[film1] = rating1;
-personalMoveDB.movies[film2] = rating2;  
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+        b = prompt('На сколько оцените его?', '');
+    if (a !== null && b !== null && a !== '' && b !== '' && a.length < 50) {
+        personalMoveDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
 
+if (personalMoveDB.count < 10) {
+    console.log('Просмотрено мало фильмов');
+} else if (personalMoveDB.count <= 30) {
+    console.log('Вы классический зритель');
+} else if (personalMoveDB.count > 30) {
+    console.log('Вы киноман');
+} else {
+    console.log('Вы киноман');
+}
+
+console.log(personalMoveDB);
