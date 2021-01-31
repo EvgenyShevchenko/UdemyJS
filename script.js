aab051b7dfb3e594193c694f9b2211c082133ccc
Evgeny Shevchenko <royalsalsk@gmail.com>
2021-02-01 00:31:04 +0300

    refactor: use methods

--- script.js
+++ script.js
@@ -1,65 +1,78 @@
 "use strict";
 
-let numberOfFilms;
-
-function start() {
-    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-    }
-}
-start();
-
 let personalMoveDB = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
-    privat: false
+    privat: false,
+    start: function () {
+        personalMoveDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+        while (personalMoveDB.count == '' || personalMoveDB.count == null || isNaN(personalMoveDB.count)) {
+            personalMoveDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+        }
+    },
+
+    rememberMyFilms: function () {
+        for (let i = 0; i < 2; i++) {
+            const a = prompt('Один из последних просмотренных фильмов?', ''),
+                b = prompt('На сколько оцените его?', '');
+            if (a !== null && b !== null && a !== '' && b !== '' && a.length < 50) {
+                personalMoveDB.movies[a] = b;
+                console.log('done');
+            } else {
+                console.log('error');
+                i--;
+            }
+        }
+    },
+
+
+    detectPersonalLevel: function () {
+        if (personalMoveDB.count < 10) {
+            console.log('Просмотрено мало фильмов');
+        } else if (personalMoveDB.count <= 30) {
+            console.log('Вы классический зритель');
+        } else if (personalMoveDB.count > 30) {
+            console.log('Вы киноман');
+        } else {
+            console.log('Произошла ошибка');
+        }
+    },
+
+    showMyDB: function (hidden) {
+        if (!hidden) {
+            console.log(personalMoveDB);
+        }
+    },
+
+    toogleVisibleMyDB: function () {
+        if (personalMoveDB.privat) {
+            personalMoveDB.privat = false;
+        } else {
+            personalMoveDB.privat = true;
+        }
+    },
+
+    writeYourGenres: function () {
+        for (let i = 1; i <= 3; i++) {
+            let genre = prompt(`Ваш любимый жанр под номером ${i}`);
+            if (genre == null || genre == '') {
+                console.log('Вы ввели некорректные данные или не ввели их вообще');
+                i--;
+            } else {
+                personalMoveDB.genres[i - 1] = genre;
+            }
+        }
+        personalMoveDB.genres.forEach((item, index) => {
+            console.log(`Любимый жанр ${index+1} - это ${item}`);
+        });
+    }
 };
 
-function rememberMyFilms() {
-    for (let i = 0; i < 2; i++) {
-        const a = prompt('Один из последних просмотренных фильмов?', ''),
-            b = prompt('На сколько оцените его?', '');
-        if (a !== null && b !== null && a !== '' && b !== '' && a.length < 50) {
-            personalMoveDB.movies[a] = b;
-            console.log('done');
-        } else {
-            console.log('error');
-            i--;
-        }
-    }
-}
 
-rememberMyFilms();
-
-function writeYourGenres() {
-    for (let i = 0; i < 3; i++) {
-        personalMoveDB.genres[i] = prompt(`Ваш любимый жанр под номером ${i+1}`);
-    }
-}
-
-writeYourGenres();
-
-function detectPersonalLevel() {
-    if (personalMoveDB.count < 10) {
-        console.log('Просмотрено мало фильмов');
-    } else if (personalMoveDB.count <= 30) {
-        console.log('Вы классический зритель');
-    } else if (personalMoveDB.count > 30) {
-        console.log('Вы киноман');
-    } else {
-        console.log('Вы киноман');
-    }
-}
-
-detectPersonalLevel();
-
-function showMyDB(hidden) {
-    if (!hidden) {
-        console.log(personalMoveDB);
-    }
-}
-
-showMyDB(personalMoveDB.privat);
+// personalMoveDB.start();
+// personalMoveDB.rememberMyFilms();
+// personalMoveDB.writeYourGenres();
+// personalMoveDB.detectPersonalLevel();
+// personalMoveDB.showMyDB(personalMoveDB.privat);
